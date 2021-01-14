268a42d46cee6836b80a155495fcca11996becd7
TorySova <toryssova@gmail.com>
2021-01-14 19:11:17 +0200

    fix login

--- src/features/Login.tsx
+++ src/features/Login.tsx
@@ -51,13 +51,8 @@ export const Login = () => {
         <Grid item xs={4}>
             <form onSubmit={formik.handleSubmit}>
                 <FormControl>
-                    <FormLabel>
-                        <p>To log in get registered
-                     <a href={'https://social-network.samuraijs.com/'}
-                                target={'_blank'}>here
-                     </a>
-                        </p>
-                        <p>or use common test account credentials:</p>
+                    <FormLabel>                   
+                        <p>Use common test account credentials:</p>
                         <p>Email: free@samuraijs.com</p>
                         <p>Password: free</p>
                     </FormLabel>
